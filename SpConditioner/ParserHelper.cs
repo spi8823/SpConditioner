87d6a78db766e916e6181cd50bedd8f06e724545
spi8823 <shin.8823@outlook.jp>
2024-09-15 16:19:20 +0900

    リファクタリング

---
+++ SpConditioner/ParserHelper.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpConditioner
+{
+    internal static class ParserHelper
+    {
+        public static Parser<Expression> chain(Parser<ExpressionType> ope, Parser<Expression> operand) => ChainOperator(ope, operand, MakeBinary).Token();
+
+        public static Parser<T> Chain<T, TOp>(Parser<TOp> op,
+                                              Parser<T> operand,
+                                              Func<TOp, T, T, T> apply)
+        {
+            return operand.Then(first => ChainRest(first, op, operand, apply));
+        }
+
+        public static Parser<T> ChainRest<T, TOp>(T firstOperand,
+                                           Parser<TOp> op,
+                                           Parser<T> operand,
+                                           Func<TOp, T, T, T> apply)
+        {
+            try
+            {
+                return op.Then(opValue => operand.Then(operandValue => ChainRest(apply(opValue, firstOperand, operandValue), op, operand, apply)));
+            }
+            catch
+            {
+                return Return(firstOperand);
+            }
+        }
+
+        public static Parser<Char> CharOrArray(string array)
+        {
+            Parser<char> result = Char(array[0]);
+            for (var i = 1; i < array.Length; i++)
+            {
+                result = result.Or(Char(array[i]));
+            }
+            return result;
+        }
+
+        public static Parser<Expression> WhereType<T>(this Parser<Expression> parser) => parser.Where(e => e.Type == typeof(T));
+    }
+}
--- SpConditioner/StatementParser.cs
+++ SpConditioner/StatementParser.cs
@@ -1,12 +1,23 @@
-﻿namespace SpConditioner
+﻿using static SpConditioner.ParserHelper;
+
+namespace SpConditioner
 {
     public static class StatementParser
     {
+        public static ParameterExpression variableParameter = Parameter(typeof(IVariableAccessor), "variable");
         public static Expression ParseToBoolExpression(string statement) => condition.Parse(statement);
+        public static Expression ParseToDoubleExpression(string statement) => addsub.Parse(statement);
+        
         public static Func<IVariableAccessor, bool> ParseToBoolFunc(string statement)
         {
             var expression = ParseToBoolExpression(statement);
             return Lambda<Func<IVariableAccessor, bool>>(expression, variableParameter).Compile();
+        }
+        
+        public static Func<IVariableAccessor, double> ParseToDoubleFunc(string statement)
+        {
+            var expression = ParseToDoubleExpression(statement);
+            return Lambda<Func<IVariableAccessor, double>>(ParseToDoubleExpression(statement), variableParameter).Compile();
         }
 
         public static bool ParseToBool(string statement, IVariableAccessor variable = null)
@@ -15,89 +26,65 @@
             return func.Invoke(variable);
         }
 
-        public static Expression ParseToDoubleExpression(string statement) => addsub.Parse(statement);
-        public static Func<IVariableAccessor, double> ParseToDoubleFunc(string statement)
+        public static double ParseToDouble(string statement, IVariableAccessor variable = null)
         {
-            var expression = ParseToDoubleExpression(statement);
-            return Lambda<Func<IVariableAccessor, double>>(ParseToDoubleExpression(statement), variableParameter).Compile();
+            var func = ParseToDoubleFunc(statement);
+            return func.Invoke(variable);
         }
 
         public static Func<IVariableAccessor, bool> CompileBoolExpression(Expression expression) => Lambda<Func<IVariableAccessor, bool>>(expression, variableParameter).Compile();
         public static Func<IVariableAccessor, double> CompileDoubleExpression(Expression expression) => Lambda<Func<IVariableAccessor, double>>(expression, variableParameter).Compile();
 
-        private static Parser<T> Chain<T, TOp>(Parser<TOp> op,
-                                              Parser<T> operand,
-                                              Func<TOp, T, T, T> apply)
-        {
-            return operand.Then(first => ChainRest(first, op, operand, apply));
-        }
-
-        private static Parser<T> ChainRest<T, TOp>(T firstOperand,
-                                           Parser<TOp> op,
-                                           Parser<T> operand,
-                                           Func<TOp, T, T, T> apply)
-        {
-            try
-            {
-                return op.Then(opValue => operand.Then(operandValue => ChainRest(apply(opValue, firstOperand, operandValue), op, operand, apply)));
-            }
-            catch
-            {
-                return Return(firstOperand);
-            }
-        }
-
-        private static Parser<Char> CharOrArray(string array)
-        {
-            Parser<char> result = Char(array[0]);
-            for(var i = 1 ; i < array.Length; i++)
-            {
-                result = result.Or(Char(array[i]));
-            }
-            return result;
-        }
-
-        private static Parser<Expression> WhereType<T>(this Parser<Expression> parser) => parser.Where(e => e.Type == typeof(T));
-
-        private static Parser<Expression> chain(Parser<ExpressionType> ope, Parser<Expression> operand) => ChainOperator(ope, operand, MakeBinary).Token();
-
-        public static ParameterExpression variableParameter = Parameter(typeof(IVariableAccessor), "variable");
-
+        #region Parser
+        #region Operator
         private static Parser<ExpressionType> Operator(ExpressionType operation, string signature) => IgnoreCase(signature).Select(_ => operation).Token();
-        
+
+        #region Arithmetic
         private static Parser<ExpressionType> add = Ref(() => Operator(ExpressionType.Add, "+"));
         private static Parser<ExpressionType> sub = Ref(() => Operator(ExpressionType.Subtract, "-"));
         private static Parser<ExpressionType> mul = Ref(() => Operator(ExpressionType.Multiply, "*"));
         private static Parser<ExpressionType> div = Ref(() => Operator(ExpressionType.Divide, "/"));
         private static Parser<ExpressionType> mod = Ref(() => Operator(ExpressionType.Modulo, "%"));
+        #endregion
 
+        #region Comparison
         private static Parser<ExpressionType> equal = Ref(() => Operator(ExpressionType.Equal, "=="));
         private static Parser<ExpressionType> notEqual = Ref(() => Operator(ExpressionType.NotEqual, "!="));
         private static Parser<ExpressionType> greater = Ref(() => Operator(ExpressionType.GreaterThan, ">"));
         private static Parser<ExpressionType> greaterEqual = Ref(() => Operator(ExpressionType.GreaterThanOrEqual, ">="));
         private static Parser<ExpressionType> less = Ref(() => Operator(ExpressionType.LessThan, "<"));
         private static Parser<ExpressionType> lessEqual = Ref(() => Operator(ExpressionType.LessThanOrEqual, "<="));
+        #endregion
 
+        #region Condition
         private static Parser<ExpressionType> and = Ref(() => Operator(ExpressionType.AndAlso, "&&"));
         private static Parser<ExpressionType> or = Ref(() => Operator(ExpressionType.OrElse, "||"));
+        #endregion
+        #endregion
 
+        #region Signature
         private const string signatureSpecialCharacters = "_＿$＄#＃（）「」『』【】";
         private static Parser<Expression> signature = from head in Letter.Or(CharOrArray(signatureSpecialCharacters)).AtLeastOnce().Text()
                                                       from next in LetterOrDigit.Or(CharOrArray(signatureSpecialCharacters)).Many().Text().Token()
                                                       select Constant(head + next);
+        #endregion
 
-        private static Parser<Expression> doubleConst = from d in Parse.Decimal.Token()
-                                                        select Constant(double.Parse(d));
-        private static Parser<Expression> doubleVariable = from variableSignature in signature.Token()
-                                                           select Call(variableParameter, typeof(IVariableAccessor).GetMethod(nameof(IVariableAccessor.GetDouble)), variableSignature);
-        private static Parser<Expression> doubleOperand = Ref(() => doubleConst.Or(doubleVariable)).Token().Named("double Operand");
-
+        #region Bool
         private static Parser<Expression> trueConst = IgnoreCase("true").Select(_ => Constant(true)).Token();
         private static Parser<Expression> falseConst = IgnoreCase("false").Select(_ => Constant(false)).Token();
         private static Parser<Expression> boolConst = Ref(() => trueConst.Or(falseConst));
         private static Parser<Expression> boolVariable = from variableSignature in signature.Token()
                                                          select Call(variableParameter, typeof(IVariableAccessor).GetMethod(nameof(IVariableAccessor.GetBool)), variableSignature);
         private static Parser<Expression> boolOperand = Ref(() => boolConst.Or(boolVariable)).Named("bool オペランド");
+        #endregion
+
+        #region Double
+        private static Parser<Expression> doubleConst = from d in Parse.Decimal.Token()
+                                                        select Constant(double.Parse(d));
+        private static Parser<Expression> doubleVariable = from variableSignature in signature.Token()
+                                                           select Call(variableParameter, typeof(IVariableAccessor).GetMethod(nameof(IVariableAccessor.GetDouble)), variableSignature);
+        private static Parser<Expression> doubleOperand = Ref(() => doubleConst.Or(doubleVariable)).Token().Named("double Operand");
+        #endregion
 
         private static Parser<Expression> bracket(Parser<Expression> exp) => (from start in Char('(').Token()
                                                                               from content in Ref(() => exp).Token()
@@ -127,5 +114,6 @@
                 and.Or(or),
                 compare.WhereType<bool>().Or(boolOperand).Or(bracket(condition))
                 )).Named("Condition");
+        #endregion
     }
 }
